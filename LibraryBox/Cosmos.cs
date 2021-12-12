a21d5622fe2373cbd2a0734df264b8b783d3da18
Mike kyle <mkyle1121@hotmail.com>
2021-12-12 14:34:06 -0600

    Add project files.

---
+++ LibraryBox/Cosmos.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBox
+{
+    public class Cosmos
+    {        
+        private static readonly ConnectionStringSettings cosmosKey = ConfigurationManager.ConnectionStrings["cosmosKey"];
+        private static readonly ConnectionStringSettings cosmosContainer = ConfigurationManager.ConnectionStrings["cosmosContainer"];
+        private static readonly ConnectionStringSettings cosmosDatabase = ConfigurationManager.ConnectionStrings["cosmosDatabase"];
+        private static readonly ConnectionStringSettings cosmosEndpointUri = ConfigurationManager.ConnectionStrings["cosmosEndpointUri"];        
+        private CosmosClient cosmosClient;
+        private Container container;
+        
+        public Cosmos()
+        {          
+            cosmosClient = new CosmosClient(cosmosEndpointUri.ConnectionString, cosmosKey.ConnectionString);
+            container = cosmosClient.GetContainer(cosmosDatabase.ConnectionString, cosmosContainer.ConnectionString);           
+        }
+        public async Task CreateItem(Book book)
+        {
+            try
+            {
+                ItemResponse<Book> bookResponse = await container.ReadItemAsync<Book>(book.Id, new PartitionKey(book.Id));
+                Console.WriteLine($"Item Already Exists: {bookResponse.Resource.Title}.");
+            }
+            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                ItemResponse<Book> bookResponse = await container.CreateItemAsync<Book>(book, new PartitionKey(book.Id));
+                Console.WriteLine($"Added Item: {bookResponse.Resource.Title}.");
+            }
+        }
+        public async Task DeleteItem(Book book)
+        {
+            try
+            {
+                ItemResponse<Book> bookResponse = await container.ReadItemAsync<Book>(book.Id, new PartitionKey(book.Id));                
+                bookResponse = await container.DeleteItemAsync<Book>(book.Id, new PartitionKey(book.Id));
+                Console.WriteLine($"Deleted Item: {book.Title}.");
+            }
+            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                Console.WriteLine($"Item Not Found: {book.Title}");
+            }
+        }
+
+
+
+    }
+}
---
+++ LibraryBox/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LibraryBox
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var client = new HttpClient();
+            var cosmos = new Cosmos();
+
+            while (true)
+            {
+                Console.WriteLine("Please Enter An ISBN: ");
+                string ISBNInput = Console.ReadLine();
+                //ISBNInput = "0393325350";
+                if (string.IsNullOrEmpty(ISBNInput))
+                {
+                    Console.WriteLine("Please Enter An ISBN And Try Again.");
+                    continue;
+                }
+                
+                var response = await client.GetAsync($"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBNInput}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+
+                var parsedContent = JObject.Parse(content);              
+                var totalItems = parsedContent["totalItems"].ToString(); 
+                
+                JToken parsedBook;
+                if (totalItems == "0")
+                {
+                    Console.WriteLine("Could Not Find Book.  Please Try Again.");
+                    continue;
+                }
+                else
+                {
+                    parsedBook = parsedContent["items"][0]["volumeInfo"];
+                }
+
+                var book = new Book
+                {
+                    Id = parsedBook["industryIdentifiers"][0]["identifier"].ToString(),
+                    Author = parsedBook["authors"][0].ToString(),
+                    Title = parsedBook["title"].ToString(),
+                    ISBN = parsedBook["industryIdentifiers"][0]["identifier"].ToString(),
+                    Date = DateTime.Now
+                };
+
+                Console.WriteLine(JsonConvert.SerializeObject(book));
+                Console.WriteLine($"Author: {book.Author}  \nTitle: {book.Title} \nISBN: {book.ISBN} \nDate: {book.Date}");   
+
+                await cosmos.CreateItem(book);
+                //await cosmos.DeleteItem(book);
+            }
+        }
+    }
+}
