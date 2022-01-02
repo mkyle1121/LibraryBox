dd02316b9264c4757542d8841bc9bafaf6b680a8
Mike kyle <mkyle1121@hotmail.com>
2022-01-02 07:53:03 -0600

    id change.

--- LibraryBox/Models/Book.cs
+++ LibraryBox/Models/Book.cs
@@ -2,7 +2,7 @@
 {
     public class Book    
     {   
-        public string Id { get; set; }
+        public string id { get; set; }
         public string Author { get; set; }
         public string Title { get; set; }
         public string Isbn { get; set; }
